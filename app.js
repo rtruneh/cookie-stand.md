eb417eed2042bf020436640a33d9829b26705746
Rosa Truneh <rosa.truneh@gmail.com>
2021-05-06 08:49:47 -0500

    working on function w/in object literal

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 "use strict"
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', ]
-
+//function to calculate random//
 function random(min, max) {
-    return Math.floor(Math.random() * max) + min;}
+    return Math.floor(Math.random() * max - min + 1) + min;}
 
 const seattle = {
     city: 'Seattle',
@@ -12,106 +12,108 @@ const seattle = {
     avgCookieSold: 6.3,
     totalDailyCookies: 0,
     custPerHour: [],
-    cookiePerHour: [],
+    cookiesPerHour: [],
     totalDailyCookies: 0,
-
-    randCookiesPerHour: random(this.minCustPerHour, this.maxCustPerHour), 
-    randCustPerHour: random(this.minCustPerHour, this.maxCustPerHour),
+    
+    randCookies: random(this.minCustPerHour, this.maxCustPerHour), 
+    randCust: random(this.minCustPerHour, this.maxCustPerHour),
 
     randCookiesPerHour: function() {
-        for (let i = 0; i < hours.length; i++) {
-           seattle.randCookiesPerHour.render; 
-        }
-    }
+                 for (let i = 0; i < hours.length; i++) {
+           console.log(this.randCookies);
+          }
+}
 };
-    
+//calling to calculate random number??
 console.log(seattle.randCookiesPerHour());
 
 
-const tokyo = {
-    city: 'Tokyo',
-    minCustPerHour: 3,
-    maxCustPerHour: 24,
-    avgCookieSold: 1.2,
-    totalDailyCookies: 0,
-    custPerHour: [],
-    cookiePerHour: [],
-    totalDailyCookies: 0,
 
-    randCookiesPerHour: random(this.minCustPerHour, this.maxCustPerHour), 
-    randCustPerHour: random(this.minCustPerHour, this.maxCustPerHour),
 
-    randCookiesPerHour: function() {
-        for (i = 0; i < hours.length; i++) {
-           return seattle.randCookiesPerHour; 
-        }
-    }
-};
+// const tokyo = {
+//     city: 'Tokyo',
+//     minCustPerHour: 3,
+//     maxCustPerHour: 24,
+//     avgCookieSold: 1.2,
+//     totalDailyCookies: 0,
+//     custPerHour: [],
+//     cookiePerHour: [],
+//     totalDailyCookies: 0,
+
+//     randCookiesPerHour: random(this.minCustPerHour, this.maxCustPerHour), 
+//     randCustPerHour: random(this.minCustPerHour, this.maxCustPerHour),
+
+//     randCookiesPerHour: function() {
+//         for (i = 0; i < hours.length; i++) {
+//            return seattle.randCookiesPerHour; 
+//         }
+//     }
+// };
     
 
 
-const dubai = {
-    city: 'Dubai',
-    minCustPerHour: 11,
-    maxCustPerHour: 38,
-    avgCookieSold: 3.7,
-    totalDailyCookies: 0,
-    custPerHour: [],
-    cookiePerHour: [],
-    totalDailyCookies: 0,
+// const dubai = {
+//     city: 'Dubai',
+//     minCustPerHour: 11,
+//     maxCustPerHour: 38,
+//     avgCookieSold: 3.7,
+//     totalDailyCookies: 0,
+//     custPerHour: [],
+//     cookiePerHour: [],
+//     totalDailyCookies: 0,
 
-    randCookiePerHour: function() {
-        console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
-    },
-    randCustPerHour: function() {
-        console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+//     randCookiePerHour: function() {
+//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+//     },
+//     randCustPerHour: function() {
+//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
 
-    },
-render() {
+//     },
+// render() {
 
-}
-};
+// }
+// };
 
-const paris = {
-    city: 'Paris',
-    minCustPerHour: 20,
-    maxCustPerHour: 38,
-    avgCookieSold: 2.3,
-    totalDailyCookies: 0,
-    custPerHour: [],
-    cookiePerHour: [],
-    totalDailyCookies: 0,
+// const paris = {
+//     city: 'Paris',
+//     minCustPerHour: 20,
+//     maxCustPerHour: 38,
+//     avgCookieSold: 2.3,
+//     totalDailyCookies: 0,
+//     custPerHour: [],
+//     cookiePerHour: [],
+//     totalDailyCookies: 0,
 
-    randCookiePerHour: function() {
-        console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
-    },
-    randCustPerHour: function() {
-        console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+//     randCookiePerHour: function() {
+//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+//     },
+//     randCustPerHour: function() {
+//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
 
-    },
-render() {
+//     },
+// render() {
 
-}
-};
+// }
+// };
 
-const lima = {
-    city: 'Lima',
-    minCustPerHour: 2,
-    maxCustPerHour: 16,
-    avgCookieSold: 4.6,
-    totalDailyCookies: 0,
-    custPerHour: [],
-    cookiePerHour: [],
-    totalDailyCookies: 0,
+// const lima = {
+//     city: 'Lima',
+//     minCustPerHour: 2,
+//     maxCustPerHour: 16,
+//     avgCookieSold: 4.6,
+//     totalDailyCookies: 0,
+//     custPerHour: [],
+//     cookiePerHour: [],
+//     totalDailyCookies: 0,
 
-    randCookiePerHour: function() {
-        console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
-    },
-    randCustPerHour: function() {
-        console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+//     randCookiePerHour: function() {
+//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+//     },
+//     randCustPerHour: function() {
+//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
 
-    },
-render() {
+//     },
+// render() {
 
-}
-};
+// }
+// };
