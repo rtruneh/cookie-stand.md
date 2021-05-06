0f9befc888ca13515d2cecd6b2369fc2bde95d54
Rosa Truneh <rosa.truneh@gmail.com>
2021-05-06 10:50:33 -0500

    finalized function and added html syntax

--- app.js
+++ app.js
@@ -14,106 +14,144 @@ const seattle = {
     custPerHour: [],
     cookiesPerHour: [],
     totalDailyCookies: 0,
-    
-    randCookies: random(this.minCustPerHour, this.maxCustPerHour), 
-    randCust: random(this.minCustPerHour, this.maxCustPerHour),
+
+    // randCookies: random(this.minCustPerHour, this.maxCustPerHour), 
+    // randCust: random(this.minCustPerHour, this.maxCustPerHour),
 
     randCookiesPerHour: function() {
                  for (let i = 0; i < hours.length; i++) {
-           console.log(this.randCookies);
+                     let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+           console.log(randNum);
           }
+        },
+
+          randCustomerPerHour: function() {
+            for (let i = 0; i < hours.length; i++) {
+                let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+      console.log(randNum);
+     }
 }
 };
 //calling to calculate random number??
-console.log(seattle.randCookiesPerHour());
+seattle.randCookiesPerHour();
+seattle.randCustomerPerHour();
 
 
 
 
-// const tokyo = {
-//     city: 'Tokyo',
-//     minCustPerHour: 3,
-//     maxCustPerHour: 24,
-//     avgCookieSold: 1.2,
-//     totalDailyCookies: 0,
-//     custPerHour: [],
-//     cookiePerHour: [],
-//     totalDailyCookies: 0,
-
-//     randCookiesPerHour: random(this.minCustPerHour, this.maxCustPerHour), 
-//     randCustPerHour: random(this.minCustPerHour, this.maxCustPerHour),
-
-//     randCookiesPerHour: function() {
-//         for (i = 0; i < hours.length; i++) {
-//            return seattle.randCookiesPerHour; 
-//         }
-//     }
-// };
-    
+const tokyo = {
+    city: 'Tokyo',
+    minCustPerHour: 3,
+    maxCustPerHour: 24,
+    avgCookieSold: 1.2,
+    totalDailyCookies: 0,
+    custPerHour: [],
+    cookiePerHour: [],
+    totalDailyCookies: 0,
 
 
-// const dubai = {
-//     city: 'Dubai',
-//     minCustPerHour: 11,
-//     maxCustPerHour: 38,
-//     avgCookieSold: 3.7,
-//     totalDailyCookies: 0,
-//     custPerHour: [],
-//     cookiePerHour: [],
-//     totalDailyCookies: 0,
+    randCookiesPerHour: function() {
+        for (let i = 0; i < hours.length; i++) {
+            let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+  console.log(randNum);
+ }
+},
 
-//     randCookiePerHour: function() {
-//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
-//     },
-//     randCustPerHour: function() {
-//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+ randCustomerPerHour: function() {
+   for (let i = 0; i < hours.length; i++) {
+       let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+console.log(randNum);
+}
+}
+};
+//calling to calculate random number??
+tokyo.randCookiesPerHour();
+tokyo.randCustomerPerHour();
 
-//     },
-// render() {
 
-// }
-// };
+const dubai = {
+    city: 'Dubai',
+    minCustPerHour: 11,
+    maxCustPerHour: 38,
+    avgCookieSold: 3.7,
+    totalDailyCookies: 0,
+    custPerHour: [],
+    cookiePerHour: [],
+    totalDailyCookies: 0,
 
-// const paris = {
-//     city: 'Paris',
-//     minCustPerHour: 20,
-//     maxCustPerHour: 38,
-//     avgCookieSold: 2.3,
-//     totalDailyCookies: 0,
-//     custPerHour: [],
-//     cookiePerHour: [],
-//     totalDailyCookies: 0,
 
-//     randCookiePerHour: function() {
-//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
-//     },
-//     randCustPerHour: function() {
-//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+    randCookiesPerHour: function() {
+        for (let i = 0; i < hours.length; i++) {
+            let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+  console.log(randNum);
+ }
+},
 
-//     },
-// render() {
+ randCustomerPerHour: function() {
+   for (let i = 0; i < hours.length; i++) {
+       let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+console.log(randNum);
+}
+}
+};
+//calling to calculate random number??
+dubai.randCookiesPerHour();
+dubai.randCustomerPerHour();
 
-// }
-// };
+const paris = {
+    city: 'Paris',
+    minCustPerHour: 20,
+    maxCustPerHour: 38,
+    avgCookieSold: 2.3,
+    totalDailyCookies: 0,
+    custPerHour: [],
+    cookiePerHour: [],
+    totalDailyCookies: 0,
 
-// const lima = {
-//     city: 'Lima',
-//     minCustPerHour: 2,
-//     maxCustPerHour: 16,
-//     avgCookieSold: 4.6,
-//     totalDailyCookies: 0,
-//     custPerHour: [],
-//     cookiePerHour: [],
-//     totalDailyCookies: 0,
 
-//     randCookiePerHour: function() {
-//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
-//     },
-//     randCustPerHour: function() {
-//         console.log(Math.floor(Math.random() * maxCustPerHour) + minCustPerHour);
+    randCookiesPerHour: function() {
+        for (let i = 0; i < hours.length; i++) {
+            let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+  console.log(randNum);
+ }
+},
 
-//     },
-// render() {
+ randCustomerPerHour: function() {
+   for (let i = 0; i < hours.length; i++) {
+       let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+console.log(randNum);
+}
+}
+};
+//calling to calculate random number??
+paris.randCookiesPerHour();
+paris.randCustomerPerHour();
 
-// }
-// };
+const lima = {
+    city: 'Lima',
+    minCustPerHour: 2,
+    maxCustPerHour: 16,
+    avgCookieSold: 4.6,
+    totalDailyCookies: 0,
+    custPerHour: [],
+    cookiePerHour: [],
+    totalDailyCookies: 0,
+
+
+    randCookiesPerHour: function() {
+        for (let i = 0; i < hours.length; i++) {
+            let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+  console.log(randNum);
+ }
+},
+
+ randCustomerPerHour: function() {
+   for (let i = 0; i < hours.length; i++) {
+       let randNum = random(this.minCustPerHour, this.maxCustPerHour)
+console.log(randNum);
+}
+}
+};
+//calling to calculate random number??
+lima.randCookiesPerHour();
+lima.randCustomerPerHour();
