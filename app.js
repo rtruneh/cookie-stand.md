dca669062cd06cb357e39eefd5b55fdd09173fcd
Rosa Truneh <rosa.truneh@gmail.com>
2021-05-11 10:20:05 -0500

    worked on getting footer to work

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 "use strict"
-
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', ]
 
 let allShops = [];
-
-
 
 //to create the constructor function for the stores
 function Shops(city, minCustPerHour, maxCustPerHour, avgCookieSold) {
@@ -17,6 +14,7 @@ function Shops(city, minCustPerHour, maxCustPerHour, avgCookieSold) {
         this.custPerHour = [];
         allShops.push(this);
 };
+
 //to calc random number
 let random = function(min, max) {
     return Math.floor(Math.random() * max - min + 1) + min;
@@ -27,7 +25,7 @@ Shops.prototype.calcCustPerHour = function() {
     for (let i = 0; i < hours.length; i++) {
         this.custPerHour.push(random(this.minCustPerHour, this.maxCustPerHour));
     } 
-};
+}
 
 //calc cookies per hour
 Shops.prototype.calcCookiesPerHour = function(){
@@ -35,10 +33,9 @@ Shops.prototype.calcCookiesPerHour = function(){
       for (let i = 0; i < hours.length; i++) {
           this.cookiesPerHour.push(Math.ceil(this.custPerHour[i] * this.avgCookieSold));
              }
-   };
+   }
 
 //calc daily total
-let totalCookies = 0;
    Shops.prototype.calcTotalDailyCookies = function() {
        let totalCookies = 0;
        for (let i = 0; i < hours.length; i++){
@@ -66,44 +63,91 @@ Shops.prototype.render = function() {
             td.textContent = this.cookiesPerHour[i];
             tr.appendChild(td);
         }
-    //show totals
-    const td2 = document.createElement('td');
-    td2.textContent = this.calcTotalDailyCookies();
-    tr.appendChild(td2);
+    //show row totals
+    const tdRowTotal = document.createElement('td');
+    tdRowTotal.textContent = this.calcTotalDailyCookies();
+    tr.appendChild(tdRowTotal);
     };
 
 
-    function header() {
+    function header() {     
+//Created header label: hours
         const trHead = document.createElement('tr');
-      
+        const thHead = document.createElement('th');
+        thHead.textContent = "Hours";
+        trHead.appendChild(thHead);
+
+    //     //create "daily total" at end of header
+    //   const tdHead2 = document.createElement('td')
+    //   tdHead2.textContent = "daily total";
+    //   trHead.appendChild(tdHead2);
+    //   table.appendChild(trHead);
+        
+//get hours to show as headers
         for(let i = 0; i < hours.length; i++){
-            const th = document.createElement('th');
-            th.textContent = hours[i];
-            table.appendChild(trHead)
-            trHead.appendChild(th);
+            const thHeader = document.createElement('th');
+            thHeader.textContent = hours[i];
+            trHead.appendChild(thHeader);
+            table.appendChild(trHead);
         }
-      };
+      }
+
+      function totalAtFooter() {
+        let shopCookiePerHour = [];
+        for (let i = 0; i < hours.length; i++) {
+            let totalCookiePerHour = 0;
+            for (let j = 0; j < allShops.length; j++) {
+                totalCookiePerHour += allShops[j].cookiesPerHr[i];
+            }
+            shopCookiePerHour.push(totalCookiePerHour);
+        }
+
+    const tblFooter = document.createElement('tfoot');
+    table.appendChild(tblFooter);
+    const trFooter = document.createElement('tr');
+    trFooter.id = 'total-row';
+    tblFooter.appendChild(trFooter);
+    let tdFooter = document.createElement('td');
+    tdFooter.textContent = 'Total';
+    trFooter.appendChild(tdFooter);
+    //total of hours
+        for (let i = 0; i < hours.length; i++) {
+        const td = document.createElement('td');
+        td.textContent = shopCookiePerHour[i];
+        trFooter.appendChild(td);
+    }
+     //total of all totals
+    let totalAllShops = 0;
+    for (let i = 0; i < hours.length; i++) {
+        totalAllShops = shopCookiePerHour[i];
+    }
+    const td = document.createElement('td');
+    td.textContent = totalAllShops;
+    trFooter.appendChild(td);
+    tblFooter.appendChild(trFooter);
+    console.log(shopCookiePerHour[i]);
+    };
+
+    totalAtFooter();
 
 
 
-      function footer(){
-          const tblFooter = document.createElement('tfoot');
-          table.appendChild(tblFooter);
-          const trFooter = document.createElement('tr');
-          tblFooter.appendChild(trFooter);
-          for(let i = 0; i < hours.length; i++){
 
-              const th = document.createElement('th');
-              th.textContent = 'total';
-            //   
-              trFooter.appendChild(th);
-          }
+//       function footer(){
+//           const tblFooter = document.createElement('tfoot');
+//           table.appendChild(tblFooter);
+//           const trFooter = document.createElement('tr');
+//           trFooter.textContent= "Totals";
+//           tblFooter.appendChild(trFooter);
           
-};
+//           for(let i = 0; i < hours.length; i++){
+//               const th = document.createElement('th');
+//               th.textContent = 'ttl';
+//               trFooter.appendChild(th);
+//           }
+// };
 
-
-
-footer();
+// footer();
 header();
 
 const seattle = new Shops('Seattle', 23, 65, 6.3);
